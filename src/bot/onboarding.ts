6c71b98fb7238691dd645b9682078a26c76197e6
Phoomparin Mano <phoomparin@gmail.com>
2019-06-22 15:55:32 +0700

    :speech_balloon: Gender consistency

--- src/bot/onboarding.ts
+++ src/bot/onboarding.ts
@@ -4,8 +4,8 @@ import {getProductsCarousel} from 'products/getProductsCarousel'
 export async function performOnboarding(ctx: BotContext)  {
   const {reply} = ctx
 
-  await reply('ร้าน Salted Egg & Bubble Tea สวัสดีครับ! 💖')
-  await reply('ลองเลือกดูสินค้าก่อนได้เลยครับ 😃')
+  await reply('ร้าน Salted Egg & Bubble Tea สวัสดีค่ะ! 💖')
+  await reply('ลองเลือกดูสินค้าก่อนได้เลยค่ะ 😃')
 
   const carousel = await getProductsCarousel()
   await reply(carousel)
